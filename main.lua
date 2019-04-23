38807b923c8f22cb0f741749dda632fb0d6526e2
lshems <g.terhorst@tabbasoum.com>
2019-04-23 19:29:09 +0200

    Update main.lua
    Complete rework. 
    Put all the initialising stuff into the create function. 
    
    No global variables,
    faster map loading,
    ...
    
    Up to you to decide what to do with it.

--- main.lua
+++ main.lua
@@ -8,162 +8,162 @@
 ---- # it under the terms of the GNU General Public License version 3 as     #
 ---- # published by the Free Software Foundation.                            #
 ---- #                                                                       #
----- # Rev 1.0  				                          									         #
+---- # Rev 0.1  				                          									         #
 ---- # Tonnie Oostbeek                     													         #
----- # special thanks to LShems for his support.                                                                      #
+---- #                                                                       #
 ---- #########################################################################
 
 
-local bmp
 local options = {
   { "TextColor", COLOR, Black }
 }
 
 
-
+-- in the create function you add all shared variables to the array containing the widget data ('thisWidget')
 local function create(zone, options)
-  local myZone  = { zone=zone, options=options, counter=0 }
-  gpsID = getFieldInfo("GPS").id	
-  HdgID = getFieldInfo("Hdg").id
-  GSpdID = getFieldInfo("GSpd").id
-  bmp0 = Bitmap.open("/Widgets/Image1/map.png")
-  bmp1 = Bitmap.open("/Widgets/Image1/map2.png")
-  bmp2 = Bitmap.open("/Widgets/Image1/map2.png")
+  local thisWidget  = { zone=zone, options=options}
   
-  lcd.drawBitmap(bmp, 0 , 0 )
+  --create array containing all sensor ID's used for quicker retrieval
+  local ID = {}
+  ID.GPS = getFieldInfo("GPS") and getFieldInfo("GPS").id	or 0
+  ID.Hdg = getFieldInfo("Hdg") and getFieldInfo("Hdg").id or 0
+  ID.GSpd = getFieldInfo("GSpd") and getFieldInfo("GSpd").id or 0
   
-  return myZone 
-  end
+  --add ID to thisWidget
+  thisWidget.ID = ID	
 
-local function background()
+  --create array containing all map info per map size
+  local map = {North={},South={},West={},East={},wx={},wy={},zx={},zy={}}
+		
+  -- coordinates for the small map.
+  map.North.small = 52.559801
+  map.South.small = 52.554921
+  map.West.small = 5.867646
+  map.East.small = 5.879677
+  map.wx.small = 320
+  map.wy.small = 0
+  map.zx.small = 479
+  map.zy.small = 210
+
+  -- coordinates for the medium map.
+  map.North.medium = 52.561551
+  map.South.medium = 52.554211
+  map.West.medium = 5.864698
+  map.East.medium = 5.882574
+  map.wx.medium = 246
+  map.wy.medium = 0
+  map.zx.medium = 443
+  map.zy.medium = 271
+
+  --coordinates for the largest map. 
+  map.North.large = 52.564116
+  map.South.large = 52.553043
+  map.West.large = 5.859864
+  map.East.large = 5.887011
+  map.wx.large = 197
+  map.wy.large = 0
+  map.zx.large = 410
+  map.zy.large = 271
+		
+  --add one bitmap per map size and set current map size
+  map.bmp={}
+  map.bmp.small = Bitmap.open("/Widgets/Image1/map.png")
+  map.bmp.medium = Bitmap.open("/Widgets/Image1/map1.png")
+  map.bmp.large = Bitmap.open("/Widgets/Image1/map2.png")
+  
+  --set current size
+  map.current = "large"
+
+  --add the map array to thisWidget
+  thisWidget.map = map	
+  
+  --return the thisWidget array to the opentx API, containing all data to be shared across functions
+  return thisWidget
 end
 
-local function update(myZone, options)
-  myZone.options = options
-end
-
-function refresh(myZone)
-  local plane = model.getInfo()
-  local style = SMLSIZE
-  local xOffset = 0
-  local yOffset = 0
+local function background(thisWidget)
   
- --  ***************  prevent from run in wrong areas ***********************	
- --[[
-    if myZone.zone.w  > 350 and myZone.zone.h > 180 then   		-- fullscreen
---  if myZone.zone.w  > 180 and myZone.zone.h > 145  then 		-- halfscreen
---  if myZone.zone.w  > 170 and myZone.zone.h > 65 then 		-- "quater"
---  if myZone.zone.w  > 150 and myZone.zone.h > 28 then 			-- all zones but not TopBar
---  if myZone.zone.w  > 65 and myZone.zone.h > 35 then 			-- TopBar
-	
-  else
-		lcd.setColor(CUSTOM_COLOR, RED)
-		lcd.drawText(myZone.zone.x + 2, myZone.zone.y+2,"select single window Widget no top bar",0)
+  thisWidget.gpsLatLong = getValue(thisWidget.ID.GPS)
+  if  (type(thisWidget.gpsLatLong) ~= "table") then
+    thisWidget.ID.GPS = getFieldInfo("GPS") and getFieldInfo("GPS").id	or 0
+    thisWidget.ID.Hdg = getFieldInfo("Hdg") and getFieldInfo("Hdg").id or 0
+    thisWidget.ID.GSpd = getFieldInfo("GSpd") and getFieldInfo("GSpd").id or 0
+    model.setGlobalVariable(8,0,0)
+    return
   end
---]]
-
   
-gpsLatLong = getValue(gpsID)
-if  (type(gpsLatLong) == "table") then 
-headingDeg= getValue(HdgID)  
-GPSSpeed = getValue(GSpdID)
-gpsLat = gpsLatLong["lat"]
-gpsLong = gpsLatLong["lon"]
-model.setGlobalVariable(7,0,1)
-
+  thisWidget.headingDeg= getValue(thisWidget.ID.Hdg)  
+  thisWidget.gpsLat = thisWidget.gpsLatLong.lat
+  thisWidget.gpsLong = thisWidget.gpsLatLong.lon
+  
 -- Part for loading the correct zoomlevel of the map
 
 -- coordinates for the smallest map. These can be found by placing the image back into Google Earth and looking at the overlay
 -- parameters
-local mapsmallNorth = 52.559801
-local mapsmallSouth = 52.554921
-local mapsmallWest = 5.867646
-local mapsmallEast = 5.879677
 
-
--- coordinates for the medium map.
-local mapmediumNorth = 52.561551
-local mapmediumSouth = 52.554211
-local mapmediumWest = 5.864698
-local mapmediumEast = 5.882574
-
---coordinates for the largest map. 
-local maplargeNorth = 52.564116
-local maplargeSouth = 52.553043
-local maplargeWest = 5.859864
-local maplargeEast = 5.887011
-
-
-
-   if      gpsLat < mapsmallNorth and gpsLat > mapsmallSouth and gpsLong < mapsmallEast and gpsLong > mapsmallWest
-    then    mapNorth = mapsmallNorth
-            mapSouth = mapsmallSouth
-            mapEast = mapsmallEast
-            mapWest = mapsmallWest
-            wx = 320
-            wy = 0
-            zx = 479
-            zy = 210
-            bmp = bmp0
-    elseif  gpsLat < mapmediumNorth and gpsLat > mapmediumSouth and gpsLong < mapmediumEast and gpsLong > mapmediumWest
-    then    mapNorth = mapmediumNorth
-            mapSouth = mapmediumSouth
-            mapEast = mapmediumEast
-            mapWest = mapmediumWest
-            wx = 246
-            wy = 0
-            zx = 443
-            zy = 271
-            bmp = bmp1
-    else    mapNorth = maplargeNorth
-            mapSouth = maplargeSouth
-            mapEast = maplargeEast
-            mapWest = maplargeWest
-            wx = 197
-            wy = 0
-            zx = 410
-            zy = 271
-            bmp = bmp2
+  local North = thisWidget.map.North
+  local South = thisWidget.map.South
+  local East = thisWidget.map.East
+  local West = thisWidget.map.West
     
-
-
-
---	return bmp, mapWest, mapEast, mapNorth, mapSouth
-  
+  if thisWidget.gpsLat < North.small and thisWidget.gpsLat > South.small and thisWidget.gpsLong < East.small and thisWidget.gpsLong > West.small then    
+    thisWidget.map.current = "small"
+  elseif thisWidget.gpsLat < North.medium and thisWidget.gpsLat > South.medium and thisWidget.gpsLong < East.medium and thisWidget.gpsLong > West.medium then    
+    thisWidget.map.current = "medium"
+  else    
+    thisWidget.map.current = "large"
   end
 
 -- Part for setting the correct zoomlevel ends here.
 
 -- Calculate Position in relation to map. 
 
+  North = North[thisWidget.map.current]
+  South = South[thisWidget.map.current]
+  East = East[thisWidget.map.current]
+  West = West[thisWidget.map.current]
+  local wx = thisWidget.map.wx[thisWidget.map.current]
+  local wy = thisWidget.map.wy[thisWidget.map.current]
+  local zx = thisWidget.map.zx[thisWidget.map.current]
+  local zy = thisWidget.map.zy[thisWidget.map.current]
 
-x = math.floor(480*((gpsLong - mapWest)/(mapEast - mapWest)))
-y = math.floor(272*((mapNorth - gpsLat)/(mapNorth - mapSouth)))
 
-if x < 10 then x = 10 
-elseif x > 470 then x = 470
-else x = x
+  thisWidget.x = math.floor(480*((thisWidget.gpsLong - West)/(East - West)))
+  thisWidget.y = math.floor(272*((North - thisWidget.gpsLat)/(North - South)))
+
+  thisWidget.x=math.max(10,thisWidget.x)
+  thisWidget.x=math.min(thisWidget.x,470)
+
+  thisWidget.y=math.max(10,thisWidget.y)
+  thisWidget.y=math.min(thisWidget.y,262)
+
+  if ((thisWidget.x - wx)*(zy-wy))-((thisWidget.y - wy)*(zx-wx)) < 0 then
+    model.setGlobalVariable(8,0,0)
+  else 
+    model.setGlobalVariable(8,0,1)
+  end
+
 end
 
-if y < 10 then y = 10
-elseif y > 262 then y = 262
-else y = y
+local function update(thisWidget, options)
+  thisWidget.options = options
 end
 
--- Part for Map position ends here
+local function refresh(thisWidget)
+  background(thisWidget)
 
+  if  (type(thisWidget.gpsLatLong) ~= "table") then
+    lcd.drawBitmap(thisWidget.map.bmp.large, thisWidget.zone.x -10, thisWidget.zone.y -10)
+    lcd.setColor(CUSTOM_COLOR, lcd.RGB(255,0,0))
+    lcd.drawText( 20, 130, "No GPS SIGNAL !!!", DBLSIZE, CUSTOM_COLOR)
+    return
+  end
 
-
-lcd.drawBitmap(bmp, myZone.zone.x - 10 , myZone.zone.y - 10 )
-
-
-lcd.setColor(CUSTOM_COLOR, RED)
-lcd.drawText(380, 30, math.floor(GPSSpeed)  .. " Km/h ", DBLSIZE + CUSTOM_COLOR)
-
-
-  xvalues = { }
-  yvalues = { }
-
+  local xvalues = { }
+  local yvalues = { }
+  local headingDeg = thisWidget.headingDeg
+  local x = thisWidget.x
+  local y = thisWidget.y
 --                     A
 --                     |
 --                     |
@@ -191,27 +191,30 @@ lcd.drawText(380, 30, math.floor(GPSSpeed)  .. " Km/h ", DBLSIZE + CUSTOM_COLOR)
   yvalues.ey = y + ((7 * math.cos(math.rad(headingDeg))) - (3 * math.sin(math.rad(headingDeg))))	-- left tailwing tip y position
   xvalues.fx = x - ((7 * math.sin(math.rad(headingDeg))) - (3 * math.cos(math.rad(headingDeg))))	-- right tailwing tip x position
   yvalues.fy = y + ((7 * math.cos(math.rad(headingDeg))) + (3 * math.sin(math.rad(headingDeg))))	-- right tailwing tip y position
-lcd.setColor(CUSTOM_COLOR, lcd.RGB(255,255,255))
-lcd.drawLine(xvalues.ax, yvalues.ay, xvalues.bx, yvalues.by, SOLID, CUSTOM_COLOR)
-lcd.drawLine(xvalues.cx, yvalues.cy, xvalues.dx, yvalues.dy, SOLID, CUSTOM_COLOR)
-lcd.drawLine(xvalues.ex, yvalues.ey, xvalues.fx, yvalues.fy, SOLID, CUSTOM_COLOR)
+  
+  
+--draw background  
+  lcd.drawBitmap(thisWidget.map.bmp[thisWidget.map.current], thisWidget.zone.x -10, thisWidget.zone.y -10)
+
+--draw info
+  lcd.setColor(CUSTOM_COLOR, RED)
+  lcd.drawText(40, 40, thisWidget.gpsLat, CUSTOM_COLOR)
+  lcd.setColor(CUSTOM_COLOR, WHITE)
+  lcd.drawText(40, 60, thisWidget.gpsLong , CUSTOM_COLOR)
+  lcd.setColor(CUSTOM_COLOR, BLUE)
+  lcd.drawText(40, 80, math.floor(thisWidget.headingDeg) , CUSTOM_COLOR)
+  lcd.drawText(40, 100, thisWidget.x , CUSTOM_COLOR)
+  lcd.drawText(40, 120, thisWidget.y , CUSTOM_COLOR)
+  
+--draw plane  
+  lcd.setColor(CUSTOM_COLOR, lcd.RGB(255,255,255))
+  lcd.drawLine(xvalues.ax, yvalues.ay, xvalues.bx, yvalues.by, SOLID, CUSTOM_COLOR)
+  lcd.drawLine(xvalues.cx, yvalues.cy, xvalues.dx, yvalues.dy, SOLID, CUSTOM_COLOR)
+  lcd.drawLine(xvalues.ex, yvalues.ey, xvalues.fx, yvalues.fy, SOLID, CUSTOM_COLOR)
 
 --draw noflightzone
-lcd.setColor(CUSTOM_COLOR, lcd.RGB(255,0,0))
-lcd.drawLine(wx, wy, zx, zy, SOLID, CUSTOM_COLOR)
-if ((x - wx)*(zy-wy))-((y - wy)*(zx-wx)) < 0 then
-      model.setGlobalVariable(8,0,0)
-else 
-      model.setGlobalVariable(8,0,1)
-  
-end
-else 
-  bmp = bmp2
-  lcd.drawBitmap(bmp, myZone.zone.x -10, myZone.zone.y -10)
   lcd.setColor(CUSTOM_COLOR, lcd.RGB(255,0,0))
-  lcd.drawText( 100, 130, "No GPS SIGNAL !!! ", DBLSIZE + BLINK + CUSTOM_COLOR)
-  model.setGlobalVariable(8,0,0)
-  model.setGlobalVariable(7,0,0)
-end
+  lcd.drawLine(thisWidget.map.wx[thisWidget.map.current], thisWidget.map.wy[thisWidget.map.current], thisWidget.map.zx[thisWidget.map.current], thisWidget.map.zy[thisWidget.map.current], SOLID, CUSTOM_COLOR)
+
 end
 return { name="Map", options=options, create=create, update=update, background=background, refresh=refresh }
